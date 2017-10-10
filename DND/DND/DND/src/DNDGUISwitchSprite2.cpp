d1fe6de5634b811fecc090e6291a04621169914c
略游 <1339484752@qq.com>
2017-10-10 18:19:38 +0800

    1

---
+++ DND/DND/DND/src/DNDGUISwitchSprite2.cpp
@@ -0,0 +1,46 @@
+#include "DNDGUISwitchSprite2.h"
+
+namespace DND
+{
+
+	SwitchSprite2* SwitchSprite2::Create(Sprite* open, Sprite* close)
+	{
+		SwitchSprite2* ret = new SwitchSprite2;
+		ret->_spr[0] = open;
+		ret->_spr[1] = close;
+		ret->_coor = Coor::Create(open->GetCoor()->GetParent());
+		ret->_spr[0]->GetCoor()->SetParent(ret->_coor);
+		ret->_spr[1]->GetCoor()->SetParent(ret->_coor);
+		ret->SetMode(Control::SWITCH);
+		return ret;
+	}
+
+	bool SwitchSprite2::_is_pickup()
+	{
+		if (IsOpen())
+			return _spr[0]->IsPickup();
+		else
+			return _spr[1]->IsPickup();
+	}
+
+	void SwitchSprite2::_update(State state)
+	{
+		if (IsOpen())
+			_spr[0]->Render();
+		else
+			_spr[1]->Render();
+	}
+
+	SwitchSprite2::SwitchSprite2()
+	{
+		_spr[0] = NULL;
+		_spr[1] = NULL;
+		_coor = NULL;
+	}
+
+	DND::Coor* SwitchSprite2::GetCoor()
+	{
+		return _coor;
+	}
+
+}
